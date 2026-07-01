a95629042c371d362463daa73667105fc5936534
BochangZhu <160828488+BochangZhu@users.noreply.github.com>
2026-06-30 18:37:58 -0700

    add initial skeleton code

---
+++ script.js
@@ -0,0 +1,13 @@
+const myLibrary = [];
+
+function Book(title = "No Name", author = "anonymous", pages = "-1") {
+    this.title = title;
+    //...
+}
+
+function addBookToLibrary() {
+    //... add book to library list
+    // track time added
+}
+
+
