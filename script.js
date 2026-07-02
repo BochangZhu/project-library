adb301b128781e1c6dabd6b4349d014633b70d13
BochangZhu <160828488+BochangZhu@users.noreply.github.com>
2026-07-02 16:50:25 -0700

    refine button style & add js emptyLibrary check func

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
+
+const library = document.querySelector(".grid-container");
+const bookCardTemp = library.querySelector("template");
+
 const myLibrary = [];
 
-function Book(title = "No Name", author = "anonymous", pages = "-1") {
+function Book(title = "No Name", author = "anonymous", pages = "-1", read= false, time_added) {
     this.title = title;
-    //...
+    this.author = author;
+    this.page = pages;
+    this.read = read;
+    this.time_added = time_added;
 }
 
 function addBookToLibrary() {
@@ -10,4 +17,17 @@ function addBookToLibrary() {
     // track time added
 }
 
+
+function checkLibEmpty(){
+    if (library.children.length == 1) {
+        const hintPara = document.createElement("p");
+        hintPara.textContent = "Nothing to look here! Click on the button below to add books📖";
+        hintPara.classList.add("hint");
+        hintPara.setAttribute("style", "position: absolute; left: 20%; top: 50%; font-size: 3.5rem");
+        library.appendChild(hintPara);
+    }
+}
+
+
+checkLibEmpty();
 
